991aa975dc9961a90989378e0c20fb327299f2a4
ALBEK ALTAY <albekaltay.aa@gmail.com>
2025-01-08 11:42:11 +0300

    updated sitemap and robots

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://test.com'
+  const baseUrl = 'https://post-applicaiton-with-nextjs-babonbo.vercel.app'
   
   return [
     {
