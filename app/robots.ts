252efd896160c50b44eb1c1ce276413ee7bce3d2
Albek Altay <albekaltay.aa@gmail.com>
2025-02-24 18:32:32 +0300

    changed project url

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/private/', '/admin/'], 
     },
-    sitemap: 'https://post-applicaiton-with-nextjs-babonbo.vercel.app/sitemap.xml',
+    sitemap: 'https://post-applicaiton-with-nextjs-lvmynrogn-albekaltays-projects.vercel.app/sitemap.xml',
   }
 } 
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://post-applicaiton-with-nextjs-babonbo.vercel.app'
+  const baseUrl = 'https://post-applicaiton-with-nextjs-lvmynrogn-albekaltays-projects.vercel.app'
   
   return [
     {
